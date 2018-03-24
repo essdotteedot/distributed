d1becbb12ba082d4cb1bb449c6140afe750849f7
essdotteedot <essdotteedot@gmail.com>
2018-03-23 20:46:28 -0400

    update appveyor file for uwt based build

--- appveyor.sh
+++ appveyor.sh
@@ -11,7 +11,7 @@ echo Installing distributed
 echo distributed checkout dir is $APPVEYOR_BUILD_FOLDER
 /usr/`whoami`/bin/opam.exe pin add distributed $APPVEYOR_BUILD_FOLDER -n -y
 /usr/`whoami`/bin/opam.exe pin add distributed-uwt $APPVEYOR_BUILD_FOLDER -n -y
-/usr/`whoami`/bin/opam.exe pin add uwt -n -y
+/usr/`whoami`/bin/opam.exe pin add uwt --dev-repo -n -y
 /usr/`whoami`/bin/opam.exe install distributed-uwt -y
 cd distributed
 jbuilder build --dev
