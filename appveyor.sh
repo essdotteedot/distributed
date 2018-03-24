eda5ef58d7f97759a709afe9267e5f07a0d1dffc
essdotteedot <essdotteedot@gmail.com>
2018-03-23 20:57:54 -0400

    update appveyor file for uwt based build

--- appveyor.sh
+++ appveyor.sh
@@ -7,6 +7,7 @@ bash ./opam64/install.sh  --prefix /usr/`whoami`
 /usr/`whoami`/bin/opam.exe init mingw 'https://github.com/fdopen/opam-repository-mingw.git' --comp "$OCAML_BRANCH"+mingw64c --switch "$OCAML_BRANCH"+mingw64c -y -a
 eval $(/usr/`whoami`/bin/ocaml-env.exe cygwin)    
 /usr/`whoami`/bin/opam.exe install depext depext-cygwinports -y
+/usr/`whoami`/bin/opam.exe opam depext conf-pkg-config -y
 echo Installing distributed
 echo distributed checkout dir is $APPVEYOR_BUILD_FOLDER
 /usr/`whoami`/bin/opam.exe pin add distributed $APPVEYOR_BUILD_FOLDER -n -y
