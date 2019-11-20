6b95a9db4cecab9f4aac0077a32eb1b3453423fc
essdotteedot <essdotteedot@gmail.com>
2019-11-20 15:31:09 -0500

    fixing appveyor build

--- appveyor.sh
+++ appveyor.sh
@@ -1,18 +1,9 @@
 set -ex
-echo Installing opam
-cd /cygdrive/c/projects
-tar -xf 'opam64.tar.xz'
-echo Username is `whoami`
-bash ./opam64/install.sh  --prefix /usr/`whoami`
-/usr/`whoami`/bin/opam.exe init mingw 'https://github.com/fdopen/opam-repository-mingw.git' --comp "$OCAML_BRANCH"+mingw64c --switch "$OCAML_BRANCH"+mingw64c -y -a
-eval $(/usr/`whoami`/bin/ocaml-env.exe cygwin)    
-/usr/`whoami`/bin/opam.exe install depext depext-cygwinports -y
-/usr/`whoami`/bin/opam.exe depext conf-pkg-config -y
 echo Installing distributed
 echo distributed checkout dir is $APPVEYOR_BUILD_FOLDER
 /usr/`whoami`/bin/opam.exe pin add distributed $APPVEYOR_BUILD_FOLDER -n -y
 /usr/`whoami`/bin/opam.exe pin add distributed-uwt $APPVEYOR_BUILD_FOLDER -n -y 
-/usr/`whoami`/bin/opam.exe install distributed-uwt -y -t
+/usr/`whoami`/bin/opam.exe install distributed-uwt -y -v -t
 cd distributed
-dune build --workspace dune-workspace.dev
+dune build
 dune build @runtest-uwt
